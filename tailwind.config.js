49285c980914e8cc46a9d254637e4360d9eb5288
1sfandyor <rapbrand06112000@gmail.com>
2024-06-11 18:13:17 -0700

    Add global config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,17 +10,33 @@ export default {
         sm: '600px',
         md: '768px',
         lg: '984px',
-        xl: '1900px',
+        xl: '1300px',
         '2xl': '1496px',
       },
 
+      colors: {
+        black: "#181818",
+        darkCyan: '#1c2121',
+        semiDarkCyan: "#252c2c",
+        gray: '#939697',
+        darkGray: '#383838',
+        orange: '#fe8630',
+        lightOrange: '#ff9549',
+        error: '#ff2b00',
+        success: '#00D154'
+      },
+
       fontFamily: {
-        primary: 'Arial, sans-serif'
+        primary: 'serif',
+        sec: ['TT TANK', 'sans-serif']
       }
     },
 
+
+
     container: {
       center: true,
+      
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
