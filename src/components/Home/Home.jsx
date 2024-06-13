501c62c718b0283afd25c098b8a33350801a441e
1sfandyor <rapbrand06112000@gmail.com>
2024-06-13 06:03:40 -0700

    Fix home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,12 @@ import Counter from '../Counter/Counter';
 const Home = () => {
     return (
         <>
-            <section className={`w-full h-80 bg-heroMobile bg-no-repeat bg-contain xs:bg-heroLarge md:bg-heroTablet lg:bg-hero lg:bg-cover lg:h-[440px] xl:bg-cover xl:h-[510px] 2xl:h-[700px]`}>
-                <div className='hidden h-full md:flex items-center mx-10 lg:mx-20 xl:mx-32 2xl:mx-80'>
+            <section className={`flex flex-col relative mb-10 `}>
+                {/* HERO IMG */}
+                <div className='overflow-hidden h-[300px] bg-cover bg-no-repeat bg-heroMobile xs:bg-heroLarge xs:h-[215px] xs:bg-cover xs:bg-[center_bottom_-2rem] md:bg-heroTablet lg:bg-hero lg:bg-cover lg:h-[350px] xl:bg-cover xl:h-[460x] 2xl:h-[530px]'></div>
+
+                {/* CONTENT */}
+                <div className='w-auto px-4 py-5 h-auto md:absolute md:left-0 md:flex flex-col items-center lg:absolute lg:bottom-8 lg:ml-14 xl:bottom-16 xl:ml-20 2xl:ml-24 2xl:bottom-40'>
                     <div className='md:flex md:flex-col md:items-start  md:justify-center'>
                         <p className='2xl:text-3xl 2xl:mb-2.5 2xl:font-medium'>Улучшим любые условия</p>
                         <h1 className='lg:text-6xl lg:font-bold lg:mb-2.5'> TANK 500</h1>
